3689ad6bd80570d8493bf8084a1dcc44daaa9def
Azvn-development <34689520+Azvn-development@users.noreply.github.com>
2021-01-19 20:31:09 +0300

    Изменение инициализатора

--- Models/Security/Context/AppIdentityDbInitializer.cs
+++ Models/Security/Context/AppIdentityDbInitializer.cs
@@ -11,24 +11,7 @@ namespace UkrStanko.Models.Security.Context
     {
         public static async Task InitializeAsync(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
         {
-            // Добавление ролей
-            AppRole adminRole = new AppRole { Name = "Администратор" };
-            AppRole managerRole = new AppRole { Name = "Менеджер" };
-
-            if (await roleManager.FindByNameAsync("Администратор") == null) await roleManager.CreateAsync(adminRole);
-            if (await roleManager.FindByNameAsync("Менеджер") == null) await roleManager.CreateAsync(managerRole);
-
-            // Добавление пользователей
-            if (await userManager.FindByNameAsync("root") == null)
-            {
-                AppUser adminUser = new AppUser { UserName = "root", Email = "root@gmail.com", PhoneNumber = "root", Role = adminRole.Name };
-                var result = await userManager.CreateAsync(adminUser, "12346Qq");
-                
-                if(result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(adminUser, "Администратор");
-                } // if
-            } // if
+            
         } // InitializeAsync
     } // UkrStankoDbInitializer
 }
